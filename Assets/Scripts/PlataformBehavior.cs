29db81d8292c7de0925fa4b9ff871aadc4f5f24a
Victor Matheus <victor.matheus.xd@gmail.com>
2020-07-08 15:28:59 -0300

    first commit

---
+++ Assets/Scripts/PlataformBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformBehavior : MonoBehaviour
+{
+
+    public float speed = 0f;
+    public float jumpForce = 0f;
+
+    public bool isOnFloor = false;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Jump();
+        rig.AddTorque(-2);
+        if (rig.velocity.y == 0) isOnFloor = true;
+    }
+
+    void Movement()
+    {
+        rig.position += new Vector2(Input.GetAxisRaw("Horizontal"), 0f) * speed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (Input.GetAxisRaw("Jump") != 0 && isOnFloor)
+        {
+            rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isOnFloor = false;
+        }
+    }
+}
